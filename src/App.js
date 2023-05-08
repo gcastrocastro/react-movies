9f755548b4792196abb353c5c4adc33adb52db49
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-08 19:46:10 -0400

    wip: set user state variable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
       <h1>App HomePage</h1>
