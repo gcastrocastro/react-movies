73f21805514f1f956be70808dbaededf40d86f04
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-10 19:05:39 -0400

    wip: loginpageuser state

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import './App.css';
 import {useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage/LoginPage';
-import NavBar from './Components/NavBar';
-import MoviesListPage from './Pages/MoviesListPage/MoviesListPage';
-import MovieDetailPage from './Pages/MovieDetailPage/MovieDetailPage';
-import ActorsListPage from './Pages/ActorsListPage/ActorsListPage';
+import NavBar from './Components/NavBar/NavBar';
+// import MoviesListPage from './Pages/MoviesListPage/MoviesListPage';
+// import MovieDetailPage from './Pages/MovieDetailPage/MovieDetailPage';
+// import ActorsListPage from './Pages/ActorsListPage/ActorsListPage';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(undefined);
   return (
-    <div className="App">
+    <main className="App">
     { user ?
       <>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={MoviesListPage}/>
-          <Route path="/movies/:movieName" element={MovieDetailPage}/>
-          <Route path="/actors" element={ActorsListPage}/>
-        </Routes>
+        <NavBar user={user}/>
+        {/* <Routes>
+          <Route path="/" element={ <MoviesListPage/>}/>
+          <Route path="/movies/:movieName" element={ <MovieDetailPage/> }/>
+          <Route path="/actors" element={ <ActorsListPage/> }/>
+        </Routes> */}
       </>
       :
       <Routes>
         <Route path="/" element={<LoginPage user={user} setUser={setUser}/>} />
       </Routes>
     }
-    </div>
+    </main>
   );
 }
 
--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,18 +1,28 @@
-export default function LoginPage({user, setUser}) {
+import {useState} from 'react';
 
-    function handleUserChange(evt) {
-        setUser(evt.target.value);
+export default function LoginPage({setUser}) {
+    const [newUser, setNewUser] = useState('');
+
+    function handleChange(evt) {
+        setNewUser(evt.target.value);
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        setUser(newUser);
+        setNewUser('');
     }
 
     return(
         <div>
-            <form onSubmit={handleUserChange}>
+            <form autoComplete="off" onSubmit={handleSubmit}>
                 <div>
                     <label>Username</label>
                     <input 
-                        value={user}
-                        onChange={setUser}
+                        name="user"
                         type="text" 
+                        value={newUser}
+                        onChange={handleChange}
                         placeholder="Enter your username here!"
                         required
                     />
