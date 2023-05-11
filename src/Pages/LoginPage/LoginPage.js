2c5ff2cf7ad451d393585907caa80967be5ce8b1
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-10 21:52:29 -0400

    adds loginpage css

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import './LoginPage.css';
 
 export default function LoginPage({setUser}) {
     const [newUser, setNewUser] = useState('');
@@ -14,20 +15,23 @@ export default function LoginPage({setUser}) {
     }
 
     return(
-        <div>
-            <form autoComplete="off" onSubmit={handleSubmit}>
-                <div>
-                    <label>Username</label>
-                    <input 
-                        type="text" 
-                        value={newUser}
-                        onChange={handleChange}
-                        placeholder="Enter your username here!"
-                        required
-                    />
-                </div>
-                <button type="submit">Sign In</button>
-            </form>
+        <div className="background-container">
+            <div className='form-container'>
+                <form autoComplete="off" onSubmit={handleSubmit}>
+                <label>Please enter a Username</label>
+                    {/* <div className="input">          */}
+                        <input 
+                            type="text" 
+                            value={newUser}
+                            onChange={handleChange}
+                            // placeholder="Enter your username here!"
+                            required
+                        />
+                    {/* </div> */}
+                    <br/>
+                    <button type="submit">Sign In</button>
+                </form>
+            </div>
         </div>
     )
 }
